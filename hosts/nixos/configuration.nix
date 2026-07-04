2e6034b27167fc8fee48f97725accde41908cf8c
torres-rodrigo <torres.costa.rodrigo@gmail.com>
2026-07-04 14:03:42 -0300

    timezone moved to its own file, may be unecessary

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -3,11 +3,11 @@
 {
   imports = [
     ./hardware-configuration.nix
+    ../../modules/nixos/base.nix
     ../../modules/nixos/users.nix
   ];
 
   networking.hostName = hostname;
-  time.timeZone = "America/Montevideo";
   i18n.defaultLocale = "en_US.UTF-8";
 
   system.stateVersion = stateVersion;
